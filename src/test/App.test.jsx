638051721f50c445ee8e87d75cc0a2599c60a80a
Jaime Tuyuc <114107467+JaimeTuyuc@users.noreply.github.com>
2023-11-26 12:18:33 -0600

    Test gh fails (#1)
    * feat: gh-test fail action
    
    * fix: restrict actions branc
    
    ---------
    
    Co-authored-by: Jaime Tuyuc <jaime.tuyuc@telus.com>

---
+++ src/test/App.test.jsx
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import App from '../App'
+
+describe('App', () => { 
+  it('should render the logo', () => {
+    render(<App />)
+
+    expect(screen.getByRole('img')).toBeInTheDocument()
+  })
+
+  it('should check the tittle of page', () => {
+    render(<App />);
+
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toHaveTextContent('Learn & Master GitHub Actions');
+  });
+
+  it('should not check the tittle of page', () => {
+    render(<App />);
+
+    expect(screen.getByRole('heading')).not.toBeInTheDocument();
+  });
+} )
